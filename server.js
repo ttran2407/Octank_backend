5800082364f2837628b0e04a5ffb42223581b3b8
Tran <tientra@a483e788dc46.ant.amazon.com>
2021-03-30 02:04:17 -0400

    4

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser=require('body-parser');
 const cors=require('cors');
 
 const app=express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
 var mysql = require('mysql');
