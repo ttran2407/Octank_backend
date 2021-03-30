fd24b7e857cc7dca9093f1523ee07ecbf46bee94
Tran <tientra@a483e788dc46.ant.amazon.com>
2021-03-30 04:29:48 -0400

    sending back result4

--- server.js
+++ server.js
@@ -29,8 +29,9 @@ con.connect(function(err) {
 app.get('/',(req,res)=>{
   con.query("SELECT * FROM student", function (err,result,fields) {
     if (err) throw err;
+    var json = JSON.parse(JSON.stringify(result))  
   })
-  var json = JSON.parse(JSON.stringify(result))
+
   console.log(json)
   res.send(json);
 })
